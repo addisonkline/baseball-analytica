68bb78e3666250d63cc5760e14b716d7886b1c3f
Addison Kline <klineaddison@gmail.com>
2022-09-19 14:35:43 -0500

    Added writer pages + other misc edits

--- foxyChangelog.js
+++ foxyChangelog.js
@@ -5,6 +5,7 @@ const changelogApp = Vue.createApp({
         return {
             testVar: 'This is a test variable',
             versions: [
+                { ver: 'v1.01', date: '09/11/2022', desc: 'Improved calculation of team strength in team projections'}, 
                 { ver: 'v1.0', date: '08/29/2022', desc: 'Created models for teams, batters, and pitchers'} 
             ]
         }
