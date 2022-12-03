2743a52674a7dba3d556edbdd6748f31976e9e58
Addison Kline <klineaddison@gmail.com>
2022-12-02 22:13:53 -0600

    Wrote post 14, other changes

--- foxyChangelog.js
+++ foxyChangelog.js
@@ -5,6 +5,8 @@ const changelogApp = Vue.createApp({
         return {
             testVar: 'This is a test variable',
             versions: [
+                { ver: 'v1.03', date: '09/27/2022', desc: 'Resolved issue of playoff games being counted'}, 
+                { ver: 'v1.02', date: '09/22/2022', desc: 'Optimized batter and pitcher projection algorithms'}, 
                 { ver: 'v1.01', date: '09/11/2022', desc: 'Improved calculation of team strength in team projections'}, 
                 { ver: 'v1.0', date: '08/29/2022', desc: 'Created models for teams, batters, and pitchers'} 
             ]
